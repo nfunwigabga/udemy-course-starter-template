705abf67f4f2977631b6dc2aae6c9a8b478734a9
Neba Funwi-gabga <neba.funwi-gabga@un.org>
2020-10-11 15:33:07 +0200

    Updated Web routes

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 
 // This uses Laravel's Single Action Controller (through the __invoke magic function)
-Route::get('{path}', 'SpaController')->where('path', '(.*)');
+Route::get('/{catchall?}', 'SpaController')
+    ->where('catchall', '^(?!api).*$');
